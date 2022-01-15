13a79f031bcd26c19f00d6526a0649487f9eaf9b
freddo1503 <fredericferrera@yahoo.fr>
2022-01-15 12:06:58 +0000

    env

--- django-app/django-app/settings.py
+++ django-app/django-app/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = dotenv_values(".env")['SECRET_KEY']
+if load_dotenv():
+    SECRET_KEY = dotenv_values(".env")['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
